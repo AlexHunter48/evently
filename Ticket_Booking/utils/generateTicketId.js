2d16679b67a57f517f3bb1bab8592aca51c37adc
AlexHunter48 <opiaprosper64@gmail.com>
2026-06-01 20:04:24 +0100

    Delete unrelated folders

--- Ticket_Booking/utils/generateTicketId.js
+++
@@ -1,9 +0,0 @@
-// Generates a unique ticket ID in the format: TKT-2024-ABC123
-
-function generateTicketId() {
-  const year = new Date().getFullYear();
-  const random = Math.random().toString(36).substring(2, 8).toUpperCase();
-  return `TKT-${year}-${random}`;
-}
-
-module.exports = generateTicketId;
