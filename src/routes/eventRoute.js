ecd3c4bbdb923a491e3211a1152fd68e0298f6d8
Onyemaechi Amaka Courage <courageonyemaechi1@gmail.com>
2026-06-07 18:46:40 -0300

    finalpush

--- src/routes/eventRoute.js
+++ src/routes/eventRoute.js
@@ -17,12 +17,12 @@ const router = express.Router();
 
 router.post("/", verifyToken, createEvent);
 router.get("/", getAllEvents);
+router.get("/saved",verifyToken, getSavedEvents);
 router.get("/organizer/:organizerId", getOrganizerEvents);
 router.get("/:id", getSingleEvent);
 router.patch("/:id",verifyToken, updateEvent);
 router.delete("/:id",verifyToken, deleteEvent);
 router.post("/saveevent/:eventId", verifyToken,saveEvent);
 router.delete("/unsave/:eventId", verifyToken,unsaveEvent);
-router.get("/saved",verifyToken, getSavedEvents);
 
 export default router;
